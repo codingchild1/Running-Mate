cfdc4f45b0907dc7e02f67e8e70d0e12d2028264
Eleen1379 <jinihipped@gmai.com>
2022-03-04 10:05:27 +0900

    jsp 다 바꿈, controller, dao, dto 다 바꿈

--- SemiProject/src/main/java/com/mulcam/run/dto/Today.java
+++ SemiProject/src/main/java/com/mulcam/run/dto/Today.java
@@ -1,5 +1,91 @@
 package com.mulcam.run.dto;
 
-public class Today {
+import java.sql.Date;
 
+public class Today {
+	String today_articleNo; 
+	String user_id;
+	String today_title; 
+	Date today_date; 
+	int today_views; 
+	int today_likes; 
+	String today_thumb;
+	String today_contents;
+	boolean warning;
+	
+	public Today() {}
+	public Today(String today_articleNo, String user_id,
+			String today_title, Date today_date, int today_views, int today_likes, 
+			String today_thumb, String today_contents, Boolean warning) {
+		this.today_articleNo = today_articleNo; 
+		this.user_id = user_id;
+		this.today_title = today_title; 
+		this.today_date = today_date; 
+		this.today_views = today_views; 
+		this.today_likes = today_likes;
+		this.today_thumb = today_thumb; 
+		this.today_contents = today_contents; 
+		this.warning = warning;
+	}
+	public Today(String user_id, String today_title, String today_contents) {
+		this.user_id = user_id;
+		this.today_title = today_title; 
+		this.today_contents = today_contents; 
+		
+	}
+	public String getToday_articleNo() {
+		return today_articleNo;
+	}
+	public void setToday_articleNo(String today_articleNo) {
+		this.today_articleNo = today_articleNo;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getToday_title() {
+		return today_title;
+	}
+	public void setToday_title(String today_title) {
+		this.today_title = today_title;
+	}
+	public Date getToday_date() {
+		return today_date;
+	}
+	public void setToday_date(Date today_date) {
+		this.today_date = today_date;
+	}
+	public int getToday_views() {
+		return today_views;
+	}
+	public void setToday_views(int today_views) {
+		this.today_views = today_views;
+	}
+	public int getToday_likes() {
+		return today_likes;
+	}
+	public void setToday_likes(int today_likes) {
+		this.today_likes = today_likes;
+	}
+	public String getToday_thumb() {
+		return today_thumb;
+	}
+	public void setToday_thumb(String today_thumb) {
+		this.today_thumb = today_thumb;
+	}
+	public String getToday_contents() {
+		return today_contents;
+	}
+	public void setToday_contents(String today_contents) {
+		this.today_contents = today_contents;
+	}
+	public boolean getWarning() {
+		return warning;
+	}
+	public void setWarning(Boolean warning) {
+		this.warning = warning;
+	}
+	
 }
--- SemiProject/src/main/java/com/mulcam/run/service/TodayServiceImpl.java
+++ SemiProject/src/main/java/com/mulcam/run/service/TodayServiceImpl.java
@@ -2,14 +2,18 @@ package com.mulcam.run.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mulcam.run.dao.TodayDAO;
 import com.mulcam.run.dto.Today;
 
 
 @Service
 public class TodayServiceImpl implements TodayService {
-
+	@Autowired 
+	TodayDAO todayDAO; 
+	
 	@Override
 	public List<Today> getSerchBoardList() throws Exception {
 		// TODO Auto-generated method stub
@@ -18,14 +22,17 @@ public class TodayServiceImpl implements TodayService {
 
 	@Override
 	public List<Today> getSelectBoard() throws Exception {
-		// TODO Auto-generated method stub
+		//하나의 게시글 선택해서 가져오기
+		//return todayDAO.selectTBoardList();
+		
+		//return todayDAO.selectTBoard(today_articleNo);
 		return null;
 	}
 
 	@Override
 	public void setInputList(Today tboard) throws Exception {
-		// TODO Auto-generated method stub
-		
+		// tboard에 작성된 제목과 내용을 가지고 dB에 넣기 
+		todayDAO.insertTBoard(tboard);
 	}
 
 	@Override
